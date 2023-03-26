de69fd1c7ccbf5e877cae514ffa58b336c382326
Ana Carolina Petrasso <carolina15petrasso@gmail.com>
2023-03-26 13:25:19 -0300

    finish Font events

--- script/main.js
+++ script/main.js
@@ -6,6 +6,10 @@ const initialize = () => {
     $("#bottom__text").value = ""
     $("#url").value = ""
     $("#font__size").value = ""
+    $("#font__selector").value = "Impact, fantasy"
+    $("#text__color").value=""
+    $("#text___bkgd").value="#ffffff" //TODO: MODIFICAR VALUE APRA QUE SEA BLANCO 
+    $("#transparent__checkbox").checked = ""
 }
 initialize()
 //Event to switch to darkmode 
@@ -66,9 +70,14 @@ $("#bottom__text__checkbox").addEventListener("input",()=>{
         $("#meme__bottom__text").classList.remove("hidden")
     }
 })
-//FONT
+
 let memeText = document.querySelectorAll('.meme__text');
 for (const element of memeText) {
+    //FONT
+    //styles 
+    $ ("#font__selector").addEventListener("input",()=>{
+        element.style.fontFamily=$ ("#font__selector").value
+    })
     //font size 
     $("#font__size").addEventListener("input", () => {
         element.style.fontSize = `${$("#font__size").value}px`
@@ -86,6 +95,46 @@ for (const element of memeText) {
     $("#alignment__right").addEventListener("click", () => {
         element.style.textAlign = "right"
     })
-
+    //COLOR & BACKGROUND
+    //color 
+    $("#text__color").addEventListener("input",()=>{
+        element.style.color = $("#text__color").value
+        $(".text__color__code").innerText = `${$("#text__color").value}`
+    })
+    //bkgd 
+    $("#text___bkgd").addEventListener("input",()=>{
+        element.style.backgroundColor = $("#text___bkgd").value
+        $(".text__bkgd__code").innerText = `${$("#text___bkgd").value}`   
+    })
+    $("#transparent__checkbox").addEventListener("input",()=>{
+        if ($("#transparent__checkbox").checked){
+            element.style.backgroundColor = "transparent"
+            $(".meme__img").style.backgroundSize ="cover"
+        }
+        else{
+            element.style.backgroundColor = $("#text___bkgd").value
+            $(".meme__img").style.backgroundSize ="contain"
+        }
+    })
+    //outline btns
+    $("#outline__light__btn").addEventListener("click",()=>{
+        element.classList.remove("outline__dark")
+        element.classList.add("outline__light")
+    })
+    $("#outline__dark__btn").addEventListener("click",()=>{
+        element.classList.remove("outline__light")
+        element.classList.add("outline__dark")
+    })
+    $("#outline__none__btn").addEventListener("click",()=>{
+        element.classList.remove("outline__dark")
+        element.classList.remove("outline__light")
+    })
+    //spacing
+    $("#spacing").addEventListener("input",()=>{
+        element.style.padding = `${$("#spacing").value}px 50px`
+    })
+    //leading
+    $("#leading").addEventListener("input",()=>{
+        element.style.lineHeight = `${$("#leading").value}`
+    })
 }
-
