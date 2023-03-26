85f96188f0acc5ec6bc5c283939387ffa1ce74dc
Ana Carolina Petrasso <carolina15petrasso@gmail.com>
2023-03-26 19:01:43 -0300

    improve js text panel

--- script/main.js
+++ script/main.js
@@ -1,8 +1,7 @@
 const $ = (selector) => document.querySelector(selector)
-
-const defoultValues = () => {
-    $("#top__text").value = ""
-    $("#bottom__text").value = ""
+const defaultValues = () => {
+    $("#top__text").value = "TOP TEXT"
+    $("#bottom__text").value = "BOTTOM TEXT"
     $("#url").value = ""
     $("#font__size").value = ""
     $("#font__selector").value = "Impact, fantasy"
@@ -10,8 +9,10 @@ const defoultValues = () => {
     $("#text___bkgd").value = "#ffffff"
     $("#transparent__checkbox").checked = ""
     $("#bkgd__color").value = "#ffffff"
+    $(".img__panel").classList.remove("hidden")
+    $(".text__panel").classList.add("hidden")
 }
-
+//___________________ IMG PANEL_______________________
 const filters = () => {
     const brightness = $("#brightness").value
     const opacity = $("#opacity").value
@@ -37,32 +38,6 @@ const resetFilters = () => {
     filters()
 }
 
-//Event to switch to darkmode 
-$("#switch-theme").addEventListener("click", () => {
-    const currentTheme = $("body").getAttribute("theme")
-    if (currentTheme) {
-        $("#lightbulb-off").classList.remove("hidden")
-        $("#lightbulb-on").classList.add("hidden")
-        $("body").removeAttribute("theme", "dark-theme")
-        $("#switch-text").innerText = "Modo oscuro"
-    }
-    else {
-        $("#lightbulb-off").classList.add("hidden")
-        $("#lightbulb-on").classList.remove("hidden")
-        $("body").setAttribute("theme", "dark-theme")
-        $("#switch-text").innerText = "Modo claro"
-    }
-})
-//Events to change between panels 
-$("#text-btn").addEventListener("click", () => {
-    $(".img__panel").classList.add("hidden")
-    $(".text__panel").classList.remove("hidden")
-})
-$("#img-btn").addEventListener("click", () => {
-    $(".text__panel").classList.add("hidden")
-    $(".img__panel").classList.remove("hidden")
-})
-// -------------------------- IMG PANEL FUNCTIONALITIES
 const imgPanel = () => {
     //URL
     $("#url").addEventListener("input", () => {
@@ -92,104 +67,132 @@ const imgPanel = () => {
     $("#reset__filters__btn").addEventListener("click", resetFilters)
 }
 
-// ------------------------- TEXT PANEL FUNCTIONALITIES
-//TOP TEXT 
-$("#top__text").addEventListener("input", () => {
+//_____________________ TEXT PANEL _________________________
+const modifyText = () => {
     $("#meme__top__text").innerText = $("#top__text").value
-})
-$("#top__text__checkbox").addEventListener("input", () => {
-    const isChecked = $("#top__text__checkbox").checked
-    if (isChecked) {
+    $("#meme__bottom__text").innerText = $("#bottom__text").value
+}
+const noText = () => {
+    if ($("#top__text__checkbox").checked) {
         $("#meme__top__text").classList.add("hidden")
     }
     else {
         $("#meme__top__text").classList.remove("hidden")
     }
-})
-//BOTTOM TEXT
-$("#bottom__text").addEventListener("input", () => {
-    $("#meme__bottom__text").innerText = $("#bottom__text").value
-})
-$("#bottom__text__checkbox").addEventListener("input", () => {
-    const isChecked = $("#bottom__text__checkbox").checked
-    if (isChecked) {
+    if ($("#bottom__text__checkbox").checked) {
         $("#meme__bottom__text").classList.add("hidden")
     }
     else {
         $("#meme__bottom__text").classList.remove("hidden")
     }
+}
+
+const textPanel = () => {
+    $("#top__text").addEventListener("input", modifyText)
+    $("#bottom__text").addEventListener("input", modifyText)
+    $("#top__text__checkbox").addEventListener("input", noText)
+    $("#bottom__text__checkbox").addEventListener("input", noText)
+    let memeText = document.querySelectorAll('.meme__text');
+    for (const element of memeText) {
+        //FONT
+        //styles 
+        $("#font__selector").addEventListener("input", () => {
+            element.style.fontFamily = $("#font__selector").value
+        })
+        //font size 
+        $("#font__size").addEventListener("input", () => {
+            element.style.fontSize = `${$("#font__size").value}px`
+        })
+        //modify aligment
+        //aligment-left
+        $("#alignment__left").addEventListener("click", () => {
+            element.style.textAlign = "left"
+        })
+        //aligment-center
+        $("#alignment__center").addEventListener("click", () => {
+            element.style.textAlign = "center"
+        })
+        //aligment-right
+        $("#alignment__right").addEventListener("click", () => {
+            element.style.textAlign = "right"
+        })
+        //COLOR & BACKGROUND
+        //color 
+        $("#text__color").addEventListener("input", () => {
+            element.style.color = $("#text__color").value
+            $(".text__color__code").innerText = `${$("#text__color").value}`
+        })
+        //bkgd 
+        $("#text___bkgd").addEventListener("input", () => {
+            element.style.backgroundColor = $("#text___bkgd").value
+            $(".text__bkgd__code").innerText = `${$("#text___bkgd").value}`
+        })
+        $("#transparent__checkbox").addEventListener("input", () => {
+            if ($("#transparent__checkbox").checked) {
+                element.style.backgroundColor = "transparent"
+                $(".meme__img").style.backgroundSize = "cover"
+            }
+            else {
+                element.style.backgroundColor = $("#text___bkgd").value
+                $(".meme__img").style.backgroundSize = "contain"
+            }
+        })
+        //outline btns
+        $("#outline__light__btn").addEventListener("click", () => {
+            element.classList.remove("outline__dark")
+            element.classList.add("outline__light")
+        })
+        $("#outline__dark__btn").addEventListener("click", () => {
+            element.classList.remove("outline__light")
+            element.classList.add("outline__dark")
+        })
+        $("#outline__none__btn").addEventListener("click", () => {
+            element.classList.remove("outline__dark")
+            element.classList.remove("outline__light")
+        })
+        //spacing
+        $("#spacing").addEventListener("input", () => {
+            element.style.padding = `${$("#spacing").value}px 50px`
+        })
+        //leading
+        $("#leading").addEventListener("input", () => {
+            element.style.lineHeight = `${$("#leading").value}`
+        })
+    }
+}
+//_______________________THEME____________________
+//to switch between dark and light mode
+$("#switch-theme").addEventListener("click", () => {
+    const currentTheme = $("body").getAttribute("theme")
+    if (currentTheme) {
+        $("#lightbulb-off").classList.remove("hidden")
+        $("#lightbulb-on").classList.add("hidden")
+        $("body").removeAttribute("theme", "dark-theme")
+        $("#switch-text").innerText = "Modo oscuro"
+    }
+    else {
+        $("#lightbulb-off").classList.add("hidden")
+        $("#lightbulb-on").classList.remove("hidden")
+        $("body").setAttribute("theme", "dark-theme")
+        $("#switch-text").innerText = "Modo claro"
+    }
 })
 
-let memeText = document.querySelectorAll('.meme__text');
-for (const element of memeText) {
-    //FONT
-    //styles 
-    $("#font__selector").addEventListener("input", () => {
-        element.style.fontFamily = $("#font__selector").value
-    })
-    //font size 
-    $("#font__size").addEventListener("input", () => {
-        element.style.fontSize = `${$("#font__size").value}px`
-    })
-    //modify aligment
-    //aligment-left
-    $("#alignment__left").addEventListener("click", () => {
-        element.style.textAlign = "left"
-    })
-    //aligment-center
-    $("#alignment__center").addEventListener("click", () => {
-        element.style.textAlign = "center"
-    })
-    //aligment-right
-    $("#alignment__right").addEventListener("click", () => {
-        element.style.textAlign = "right"
-    })
-    //COLOR & BACKGROUND
-    //color 
-    $("#text__color").addEventListener("input", () => {
-        element.style.color = $("#text__color").value
-        $(".text__color__code").innerText = `${$("#text__color").value}`
-    })
-    //bkgd 
-    $("#text___bkgd").addEventListener("input", () => {
-        element.style.backgroundColor = $("#text___bkgd").value
-        $(".text__bkgd__code").innerText = `${$("#text___bkgd").value}`
-    })
-    $("#transparent__checkbox").addEventListener("input", () => {
-        if ($("#transparent__checkbox").checked) {
-            element.style.backgroundColor = "transparent"
-            $(".meme__img").style.backgroundSize = "cover"
-        }
-        else {
-            element.style.backgroundColor = $("#text___bkgd").value
-            $(".meme__img").style.backgroundSize = "contain"
-        }
-    })
-    //outline btns
-    $("#outline__light__btn").addEventListener("click", () => {
-        element.classList.remove("outline__dark")
-        element.classList.add("outline__light")
-    })
-    $("#outline__dark__btn").addEventListener("click", () => {
-        element.classList.remove("outline__light")
-        element.classList.add("outline__dark")
-    })
-    $("#outline__none__btn").addEventListener("click", () => {
-        element.classList.remove("outline__dark")
-        element.classList.remove("outline__light")
-    })
-    //spacing
-    $("#spacing").addEventListener("input", () => {
-        element.style.padding = `${$("#spacing").value}px 50px`
-    })
-    //leading
-    $("#leading").addEventListener("input", () => {
-        element.style.lineHeight = `${$("#leading").value}`
-    })
-}
-const inicializar = () => {
-    defoultValues()
+//__________________PANELS_________________________
+//to switch between img panel and text panel
+$("#text-btn").addEventListener("click", () => {
+    $(".img__panel").classList.add("hidden")
+    $(".text__panel").classList.remove("hidden")
+})
+$("#img-btn").addEventListener("click", () => {
+    $(".text__panel").classList.add("hidden")
+    $(".img__panel").classList.remove("hidden")
+})
+
+const initialize = () => {
+    defaultValues()
     resetFilters()
     imgPanel()
+    textPanel()
 }
-window.addEventListener("load", inicializar)
+window.addEventListener("load", initialize)
